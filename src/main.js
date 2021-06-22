72f4cd90f7b0576966e963649bc8ac9109c8eea5
ed1lor <vanndyu@gmail.com>
2021-06-22 21:40:30 +0700

    The start of the project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuesax from 'vuesax'
 
 createApp(App).use(router).mount('#app')
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax, {
+  // options here
+})
